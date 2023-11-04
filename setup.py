ca2c6eecb6d7950d2f7f02f28d48029657c45b73
ly87muro <75542844+ly87muro@users.noreply.github.com>
2023-11-04 16:07:21 +0100

    init, requirements and setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='1.0',
+    packages=['math_quiz'],
+    url='',
+    license='',
+    author='Miriam Kraft',
+    author_email='miriam.kraft@fau.de',
+    description='A simple math quiz'
+)
